f0235038d8401f2bc33cb5c8324f5bec024379e7
CO722G1-NguyenDuyDung <duydungne10@gmail.com>
2022-11-03 15:58:05 +0700

    nop bai

--- src/ss9/exercise/product_discount_calculator/src/main/java/DiscountServlet.java
+++ src/ss9/exercise/product_discount_calculator/src/main/java/DiscountServlet.java
@@ -9,13 +9,13 @@ import java.io.PrintWriter;
 @WebServlet(name = "DiscountServlet ", value = "/shopping")
 public class DiscountServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        String product_description = request.getParameter("product-description");
+        String productDescription = request.getParameter("product-description");
         float listPrice = Float.parseFloat(request.getParameter("list-price"));
         float discountPercent = Float.parseFloat(request.getParameter("discount-percent"));
         float discountAmount =  (listPrice * discountPercent * 0.01f);
         float discountPrice = listPrice - discountAmount;
 
-        request.setAttribute("product_description", product_description);
+        request.setAttribute("product_description", productDescription);
         request.setAttribute("list_price",listPrice);
         request.setAttribute("discount_amount",discountAmount);
         request.setAttribute("discount_price",discountPercent);
